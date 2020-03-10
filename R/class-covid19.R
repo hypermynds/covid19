6e116485250b0a4d67812e8e84bbed76d28197bd
msaltieri <michelestefano.altieri@gmail.com>
2020-03-11 00:14:48 +0100

    Add forecast analysis

--- R/class-covid19.R
+++ R/class-covid19.R
@@ -4,6 +4,9 @@
 #' This object type is used to retrieve, analyse and plot international data on
 #' COVID19 outbreak and spreading.
 #'
+#' @param log is a boolean; if \code{TRUE}, a logarithmic scale is applied to y
+#' axis in plots.
+#'
 #' @import highcharter
 #' @export
 Covid <- R6::R6Class(
@@ -68,6 +71,35 @@ Covid <- R6::R6Class(
         },
 
         #' @description
+        #' This is to predict the behavior on the following days.
+        #' @param n is an integer; the number of days to forecast.
+        forecast = function(n = 5L) {
+            if (is.null(private$tab))
+                stop('Nothing to forecast.\n')
+            # Predict the total number of cases
+            fit1 <- lm(log(totale_casi) ~ data, data = private$tab)
+            # Predict the total number of deaths
+            fit2 <- lm(log(deceduti) ~ data, data = private$tab)
+            # Forecast on the following n days
+            new_dates <-
+                seq(
+                    max(private$tab$data) + 1,
+                    by = '1 day',
+                    length.out = n
+                )
+            private$tab %>%
+                dplyr::select(data, totale_casi, deceduti) %>%
+                dplyr::bind_rows(
+                    dplyr::tibble(
+                        data = new_dates,
+                        totale_casi = predict(fit1, new_dates) %>% exp(),
+                        deceduti = predict(fit2, new_dates) %>% exp()
+                    ) %>%
+                        dplyr::mutate_if(is.double, as.integer)
+                )
+        },
+
+        #' @description
         #' This is used to export a plottable table
         table = function() {
             if (is.null(private$tab))
@@ -83,48 +115,82 @@ Covid <- R6::R6Class(
 
         #' @description
         #' This plots a barchart of the summary.
-        plot = function() {
+        plot_hist = function(log = FALSE) {
             if (is.null(private$tab))
                 stop('Nothing to plot.\n')
-            highchart() %>%
+            hc <-
+                highchart() %>%
                 hc_chart(type = 'column') %>%
                 hc_xAxis(
                     categories = private$tab$data %>%
                         format('%e %b')
                 ) %>%
-                hc_plotOptions(series = list(stacking = 'normal')) %>%
-                hc_legend(itemStyle = list(fontSize = '10px')) %>%
-
+                hc_plotOptions(series = list(stacking = 'normal'))
+            if (log) {
+                hc %<>%
+                    hc_yAxis(
+                        type = 'logarithmic'
+                    )
+            }
+            hc %<>%
                 # Deceduti
                 hc_add_series(
                     data = private$tab$deceduti,
                     name = 'Deaths'
                 ) %>%
-
                 # Terapia intensiva
                 hc_add_series(
                     data = private$tab$terapia_intensiva,
                     name = 'Intensive Care'
                 ) %>%
-
                 # Ricoverati con sintomi
                 hc_add_series(
                     data = private$tab$ricoverati_con_sintomi,
                     name = 'Hospitalized (no IC)'
                 ) %>%
-
                 # Isolamento domiciliare
                 hc_add_series(
                     data = private$tab$isolamento_domiciliare,
                     name = 'Home Isolation'
                 ) %>%
-
                 # Dimessi guariti
                 hc_add_series(
                     data = private$tab$dimessi_guariti,
                     name = 'Recovered'
                 )
+            hc
+        },
 
+        #' @description
+        #' This plots a linechart of the forecast.
+        plot_fore = function(log = FALSE) {
+            if (is.null(private$tab))
+                stop('Nothing to plot.\n')
+            tbl_forecast <- self$forecast()
+            hc <-
+                highchart() %>%
+                hc_xAxis(
+                    categories = tbl_forecast$data %>%
+                        format('%e %b')
+                )
+            if (log) {
+                hc %<>%
+                    hc_yAxis(
+                        type = 'logarithmic'
+                    )
+            }
+            hc %<>%
+                # Deceduti
+                hc_add_series(
+                    data = tbl_forecast$deceduti,
+                    name = 'Deaths'
+                ) %>%
+                # Terapia intensiva
+                hc_add_series(
+                    data = tbl_forecast$totale_casi,
+                    name = 'Total Cases'
+                )
+            hc
         }
 
     ),
